dd7ce76c590fa372b34c543b30e37f58ae10fea5
Alvaro Padilla <monster_what@hotmail.com>
2023-08-22 20:59:54 -0600

    First commit

---
+++ Azure-Csharp/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Azure_Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.libreriaTxt = new System.Windows.Forms.TextBox();
+            this.ArchivoTxt = new System.Windows.Forms.TextBox();
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.examButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // libreriaTxt
+            // 
+            this.libreriaTxt.Location = new System.Drawing.Point(96, 86);
+            this.libreriaTxt.Name = "libreriaTxt";
+            this.libreriaTxt.Size = new System.Drawing.Size(430, 20);
+            this.libreriaTxt.TabIndex = 0;
+            // 
+            // ArchivoTxt
+            // 
+            this.ArchivoTxt.Location = new System.Drawing.Point(96, 136);
+            this.ArchivoTxt.Name = "ArchivoTxt";
+            this.ArchivoTxt.Size = new System.Drawing.Size(430, 20);
+            this.ArchivoTxt.TabIndex = 1;
+            // 
+            // urlText
+            // 
+            this.urlText.Location = new System.Drawing.Point(96, 37);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(430, 20);
+            this.urlText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SiteURL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Libreria";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Archivo";
+            // 
+            // examButton
+            // 
+            this.examButton.Location = new System.Drawing.Point(451, 162);
+            this.examButton.Name = "examButton";
+            this.examButton.Size = new System.Drawing.Size(75, 23);
+            this.examButton.TabIndex = 6;
+            this.examButton.Text = "Examinar";
+            this.examButton.UseVisualStyleBackColor = true;
+            this.examButton.Click += new System.EventHandler(this.examButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(41, 229);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancelar";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(451, 229);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 8;
+            this.sendButton.Text = "Enviar";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 264);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.examButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.urlText);
+            this.Controls.Add(this.ArchivoTxt);
+            this.Controls.Add(this.libreriaTxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox libreriaTxt;
+        private System.Windows.Forms.TextBox ArchivoTxt;
+        private System.Windows.Forms.TextBox urlText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button examButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Azure-Csharp/Form1.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.SharePoint.Client;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Azure_Csharp
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            subirArchivo();
+        }
+
+        private void examButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = @"C:/";
+            this.openFileDialog1.Title = "Seleccione un archivo";
+            this.openFileDialog1.FileName = "*.*";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog1.FileName;
+                ArchivoTxt.Text = selectedFilePath;
+            }
+        }
+
+        public void subirArchivo()
+        {
+            string siteUrl = "https://{your_tenant}.sharepoint.com/sites/{your_site_name}"; //Nombre del sitio de SharePoint
+            string libraryName = "Documentos"; // Nombre de la biblioteca en la que deseas subir el archivo
+            string fileName = "test.pdf"; // Nombre del archivo que deseas subir
+            string filePath = "C:\\Users\\USER\\Desktop"; // Ruta local del archivo
+            string clientId = "your_app_client_id";// ID de cliente de la aplicación en Azure AD
+            string clientSecret = "valor_secreto_del_app";// Secreto de cliente de la aplicación en Azure AD
+
+            var app = ConfidentialClientApplicationBuilder.Create(clientId)
+                .WithClientSecret(clientSecret)
+                .WithAuthority(new Uri("https://login.microsoftonline.com/{your_tenant}.onmicrosoft.com/"))
+                .Build();
+
+            string[] scopes = new string[] { "https://{your_tenant}.sharepoint.com/.default" };
+            var result = app.AcquireTokenForClient(scopes).ExecuteAsync().Result;
+
+            using (ClientContext context = new ClientContext(siteUrl))
+            {
+                context.ExecutingWebRequest += (s, e) =>
+                {
+                    e.WebRequestExecutor.RequestHeaders["Authorization"] = "Bearer " + result.AccessToken;
+                };
+
+                Web web = context.Web;
+                List documentsLibrary = web.Lists.GetByTitle(libraryName);
+
+                context.Load(documentsLibrary.RootFolder);
+                context.ExecuteQuery();
+
+                // Crea el archivo en la biblioteca de SharePoint
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                {
+                    FileCreationInformation fileInfo = new FileCreationInformation
+                    {
+                        ContentStream = fs,
+                        Url = Path.Combine(documentsLibrary.RootFolder.ServerRelativeUrl, fileName),
+                        Overwrite = true
+                    };
+
+                    Microsoft.SharePoint.Client.File newFile = documentsLibrary.RootFolder.Files.Add(fileInfo);
+                    context.Load(newFile);
+                    context.ExecuteQuery();
+
+                    MessageBox.Show("Archivo subido correctamente.");
+                }
+            }
+        }
+    }
+}
